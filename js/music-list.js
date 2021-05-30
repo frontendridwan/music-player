0f9576a6efb9f85d8846b913c4748e47b083be80
Muhammad Ridwan <muh.ridwan.777@gmail.com>
2021-05-30 09:45:03 +0700

    add project

---
+++ js/music-list.js
@@ -0,0 +1,56 @@
+// To add more song, just copy the following code and paste inside the array
+
+//   {
+//     name: "Here is the music name",
+//     artist: "Here is the artist name",
+//     img: "image name here - remember img must be in .jpg formate and it's inside the images folder of this project folder",
+//     src: "music name here - remember img must be in .mp3 formate and it's inside the songs folder of this project folder"
+//   }
+
+//paste it inside the array as more as you want music then you don't need to do any other thing
+
+let allMusic = [
+  {
+    name: "Palestine Will Be Free",
+    artist: "Maher Zain",
+    img: "MaherZain-PalestineWillBeFree",
+    src: "MaherZain-PalestineWillBeFree"
+  },
+  {
+    name: "Merah Saga",
+    artist: "Shoutul Harokah",
+    img: "ShoutulHarokah-MerahSaga",
+    src: "ShoutulHarokah-MerahSaga"
+  },
+  {
+    name: "Palestina Tercinta",
+    artist: "Shoutul Harokah",
+    img: "ShoutulHarokah-PalestinaTercinta",
+    src: "ShoutulHarokah-PalestinaTercinta"
+  },
+  // {
+  //   name: "Hardwind - Want Me",
+  //   artist: "Mike Archangelo",
+  //   img: "music-4",
+  //   src: "music-4"
+  // },
+  // {
+  //   name: "Jim - Sun Goes Down",
+  //   artist: "Jim Yosef x Roy",
+  //   img: "music-5",
+  //   src: "music-5"
+  // },
+  // {
+  //   name: "Lost Sky - Vision NCS",
+  //   artist: "NCS Release",
+  //   img: "music-6",
+  //   src: "music-6"
+  // },
+  // like this paste it and remember to give comma after ending of this bracket }
+  // {
+  //   name: "Here is the music name",
+  //   artist: "Here is the artist name",
+  //   img: "image name here - remember img must be in .jpg formate and it's inside the images folder of this project folder",
+  //   src: "music name here - remember img must be in .mp3 formate and it's inside the songs folder of this project folder"
+  // }
+];
